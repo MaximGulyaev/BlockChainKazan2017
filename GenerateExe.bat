4045d6a2c91c8e6e8260a36fc3edbc1aa1df6fb0
MaximGulyaev <max_rusz@mail.ru>
2017-12-11 20:25:41 +0300

    fixes in bush

--- GenerateExe.bat
+++ GenerateExe.bat
@@ -2,7 +2,9 @@ rd dist /S
 rd build /S
 pyinstaller main.py
 cd resourse\
-xcopy * ..\dist\main\resourse /H /E /G /Q /R /Y
+xcopy * ..\dist\main\resourse\ /H /E /G /Q /R /Y
 cd ..\keys\
-xcopy * ..\dist\main\keys /H /E /G /Q /R /Y
+xcopy * ..\dist\main\keys\ /H /E /G /Q /R /Y
+cd ..\dist\main\PyQt5\Qt\plugins\platforms\
+xcopy * ..\..\..\..\platforms\ /H /E /G /Q /R /Y
 pause
